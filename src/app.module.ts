3aa96c1f9cc90a516887d2ebfe97a68b91de2875
Bohdan-Dubravin <dubravin111@gmail.com>
2023-05-01 18:52:29 +0300

    add docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from './config/orm.config';
-import ormConfigProd from './config/orm.config.prod';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useFactory:
-        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+        // process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
+        ormConfig,
     }),
   ],
   controllers: [AppController],
--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -5,11 +5,15 @@ export default registerAs(
   'orm.config',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: 'myuser',
+    password: process.env.POSTGRES_PASSWORD,
+    // password: 'mypassword',
+    database: process.env.POSTGRES_DB,
+
+    // database: 'ecommerse',
+
     entities: [],
     synchronize: true,
     dropSchema: Boolean(parseInt(process.env.DB_DROP_SCHEMA)),
